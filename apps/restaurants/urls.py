0b7738c2224dcda9c2b9bbfa773d23fea2168caa
Matt McCarthy <mattmc318@gmail.com>
2017-12-20 16:20:18 -0600

    added 'My Restaurants' page

--- apps/restaurants/urls.py
+++ apps/restaurants/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.show_restaurants, name="restaurant_home"),
-    url(r'/(?P<rest_id>\d+)$', views.rest_profile, name="rest_profile"),
-    url(r'add_restaurant$', views.add_restaurant),
-    url(r'add_location$', views.add_location),
+    url(r'(?P<rest_id>\d+)$', views.rest_profile, name="rest_profile"),
+    url(r'add_restaurant$', views.add_restaurant, name="add_restaurant"),
+    url(r'add_location$', views.add_location, name="add_location"),
 ]
