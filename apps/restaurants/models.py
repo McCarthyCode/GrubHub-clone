6790c42765c2539173e16832cdd3788932804962
Portismouth <ozzy.gonzalez@gmail.com>
2017-12-20 15:49:00 -0600

    completed restaurant CRUD funcationality

--- apps/restaurants/models.py
+++ apps/restaurants/models.py
@@ -25,6 +25,23 @@ class RestaurantManager(models.Manager):
             return (True, all_restaurants)
         return (False, errors)
 
+    def update_restaurant(self, postData, categories, value):
+        rest_updating = Restaurant.objects.get(id=postData['rest_id'])
+        errors = []
+        if len(postData['rest_name']) < 1:
+            errors.append("Please enter a Restaurant Name")
+        if not errors:
+            rest_updating.rest_name = postData['rest_name']
+            rest_updating.save()
+            if value == 'add':
+                for cat in categories:
+                    rest_updating.category.add(cat)
+            if value == 'remove':
+                for cat in categories:
+                    rest_updating.category.remove(cat)
+            rest_updating.save()
+            return (True, rest_updating)
+        return (False, errors)
 
 class RestaurantAddressManager(models.Manager):
     def address_validator(self, postData):
--- apps/restaurants/urls.py
+++ apps/restaurants/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     url(r'^$', views.show_restaurants, name="restaurant_home"),
     url(r'/(?P<rest_id>\d+)$', views.rest_profile, name="rest_profile"),
     url(r'add_restaurant$', views.add_restaurant),
+    url(r'update_restaurant$', views.update_restaurant),
+    url(r'delete_restaurant$', views.destroy_restaurant),
     url(r'add_location$', views.add_location),
 ]
