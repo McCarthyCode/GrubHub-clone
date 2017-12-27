37faafca7549265fa5b3b7ad48568a883d9f3e0e
Matt McCarthy <mattmc318@gmail.com>
2017-12-27 14:50:40 -0600

    changed navbar in restaurants app; updated show.html

--- grubhub/settings.py
+++ grubhub/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+# Link static files to path
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join('static'), )
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
